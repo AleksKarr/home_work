c6ff21dfc1a0b50884a3543363cb1945a4c1f9af
AleksKarr <a.karagodin.001@gmail.com>
2026-04-19 18:23:46 +0300

    Add file1

--- 2_hw.py
+++ 2_hw.py
@@ -1,1 +1,30 @@
-print('Hello World')
+def task_1() -> None:
+    age: int = 25
+    price: float = 19.99
+    name: str = "Алекс"
+    items: list = [1, 2, 3]
+    is_active: bool = True
+
+    print(type(age))
+    print(type(price))
+    print(type(name))
+    print(type(items))
+    print(type(is_active))
+
+
+task_1()
+
+
+def task_2() -> None:
+    a: list[int] = [1, 2, 3, 5, 8, 13, 21]
+    print(a[:3])
+
+
+task_2()
+# Это последовательность Фибоначчи
+
+def task_3(num: int) -> int:
+    return num ** 2
+
+
+print(task_3(5))
